4b0dfa1395690d5cf8503214f4c55dc38f138155
chebishev <atanas.chebishev@gmail.com>
2026-02-02 08:56:50 +0200

    add SQLAlchemy, add db support sqlite

--- tgapp.py
+++ tgapp.py
@@ -1,4 +1,6 @@
 from tg import expose, TGController, AppConfig
+from tg.util import Bunch
+from sqlalchemy.orm import sessionmaker, scoped_session
 from wsgiref.simple_server import make_server
 import webhelpers2
 import webhelpers2.text
@@ -18,6 +20,18 @@ config.renderers = ['kajiki']
 config['helpers'] = webhelpers2
 config.serve_static = True
 config.paths['static_files'] = 'public'
+config['use_sqlalchemy'] = True
+config['sqlalchemy.url'] = 'sqlite:///mydatabase.db'
+
+DBSession = scoped_session(sessionmaker(autoflush=True, autocommit=False))
+
+def init_model(engine):
+    DBSession.configure(bind=engine)
+
+config['model'] = Bunch(
+    DBSession=DBSession, 
+    init_model=init_model
+    )
 application = config.make_wsgi_app()
 
 print("Serving on http://localhost:8080")
